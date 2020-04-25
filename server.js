a5f979b2025525c8d6b867aaa813ce9034f97355
OAhonen <olli.p.ahonen@tuni.fi>
2020-04-25 13:03:52 +0300

    Updata server.js.

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/Simple-Quiz-App'));
 
-app.get('/*', function (request, response) {
-  response.sendFile(path.join(__dirname, '/dist/index.html'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname,'/dist/Simple-Quiz-App/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
