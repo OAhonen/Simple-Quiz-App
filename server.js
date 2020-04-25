990720acc419321033e4a37129ddf3f3ad2d153b
OAhonen <olli.p.ahonen@tuni.fi>
2020-04-25 12:37:49 +0300

    Update.

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
-const ngApp = express();
 
-ngApp.use(express.static('./dist/angular-forms-validation'));
-ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/angular-forms-validation/index.html'));
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/Simple-Quiz-App'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/Simple-Quiz-App/index.html'));
 });
 
-ngApp.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
