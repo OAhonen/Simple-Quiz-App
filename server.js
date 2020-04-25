5dc81c4461de5aaf5ae88845e5c0c83485ba569d
OAhonen <olli.p.ahonen@tuni.fi>
2020-04-25 12:58:58 +0300

    Update server.js.

--- server.js
+++ server.js
@@ -7,5 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/*', function (request, response) {
+  response.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
