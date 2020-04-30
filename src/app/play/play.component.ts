bd4ca11a97b95e71baade9283da4ff2acfb9e5c6
OAhonen <olli.p.ahonen@tuni.fi>
2020-04-30 08:42:16 +0300

    Add ability to answer questions.
    Show also amount of correct answers.

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -4,22 +4,62 @@ import { PlayService } from '../playservice';
 
 @Component({
   selector: 'app-play',
-  template: `<div *ngIf="questions.length">
+  template: `
+  <div *ngIf="!quizOver">Question {{curQuestionIndex + 1}} of {{questions.length}}</div>
+  <div *ngIf="quizOver">Quiz finished!</div>
+  <div *ngIf="quizOver">You got {{correctAnswers}} correct answers out of {{questions.length}} questions.</div>
 
-  <h2>Questions</h2>
-  <div *ngFor='let question of questions'> {{question.question}} {{question.incorrect_answers}} </div>
-
-</div>`
+  <div *ngFor="let question of questions; let i = index;">
+  <div *ngIf="curQuestionIndex === i">
+    <h3>{{question.question}}</h3>
+    <ul>
+      <li
+        *ngFor="let answer of question.all_answers"
+        (click)="clicked(answer)">{{answer}}</li>
+    </ul>
+  </div>
+  </div>`
 })
 export class PlayComponent implements OnInit {
-  hero = 'Windstrom';
   questions: Question[] = [];
+  correctAnswers: number;
+  curQuestionIndex: number;
+  quizOver: boolean;
 
   constructor(private playService: PlayService) { }
 
   ngOnInit(): void {
-    if (this.questions.length === 0) {
-      this.playService.fetchQuestions((result) => this.questions = result);
+    this.correctAnswers = 0;
+    this.curQuestionIndex = 0;
+    this.quizOver = false;
+    this.playService.fetchQuestions((result) => {
+      this.questions = result;
+      for (const question of this.questions) {
+        question.all_answers = question.incorrect_answers;
+        question.all_answers.push(question.correct_answer);
+        this.shuffle(question.all_answers);
+      }
+    });
+  }
+
+  shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+
+  clicked(answer: string) {
+    console.log(answer);
+    console.log(this.questions[this.curQuestionIndex].correct_answer);
+
+    if (answer === this.questions[this.curQuestionIndex].correct_answer) {
+      console.log('yes');
+      this.correctAnswers++;
+    } else {
+      console.log('no');
+    }
+    this.curQuestionIndex++;
+
+    if (this.curQuestionIndex === this.questions.length) {
+      this.quizOver = true;
     }
   }
 
